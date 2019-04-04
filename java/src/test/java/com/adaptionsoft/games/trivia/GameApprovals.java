1861b7c499e8edad9d9b66a256d5735d2e31adac
nawhi <25394965+nawhi@users.noreply.github.com>
2019-04-04 13:46:38 +0100

    Extract stdout capture logic into separate method

--- java/src/test/java/com/adaptionsoft/games/trivia/GameApprovals.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameApprovals.java
@@ -7,20 +7,29 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.Assert.assertEquals;
-
 public class GameApprovals {
     @Test
     public void add_a_player() {
+        String output = catchSystemOutFor(() -> new Game().add("Test"));
+        Approvals.verify(output);
+    }
+
+    private static String catchSystemOutFor(Runnable action) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream outputStream = new PrintStream(baos);
+
+        PrintStream originalOut = System.out;
         System.setOut(outputStream);
 
-        Game game = new Game();
+        try {
+            action.run();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            System.out.flush();
+            System.setOut(originalOut);
+        }
 
-        game.add("Test");
-
-        System.out.flush();
-        Approvals.verify(baos.toString());
+        return baos.toString();
     }
 }
