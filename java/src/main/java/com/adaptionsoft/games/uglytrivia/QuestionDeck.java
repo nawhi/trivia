01f1226c203f47f9b381d7b7e4b16d2a24ff49e4
nawhi <25394965+nawhi@users.noreply.github.com>
2019-04-25 11:52:45 +0100

    Make QuestionDeck linked list private

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
@@ -3,7 +3,7 @@ package com.adaptionsoft.games.uglytrivia;
 import java.util.LinkedList;
 
 public class QuestionDeck {
-    LinkedList questions;
+    private LinkedList questions;
     private String category;
     private int questionIndex;
 
