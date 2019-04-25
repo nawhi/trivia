e674bfe846bf5cbea938d94621506e2cc1cb889d
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-04-25 11:54:45 +0100

    Make QuestionSelector LinkedList generic

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
@@ -3,18 +3,18 @@ package com.adaptionsoft.games.uglytrivia;
 import java.util.LinkedList;
 
 public class QuestionDeck {
-    private LinkedList questions;
+    private LinkedList<String> questions;
     private String category;
     private int questionIndex;
 
     public QuestionDeck(String category) {
         questionIndex = 0;
         this.category = category;
-        questions = new LinkedList();
+        questions = new LinkedList<>();
     }
 
     public String next() {
-        return questions.removeFirst().toString();
+        return questions.removeFirst();
     }
 
     public void add() {
