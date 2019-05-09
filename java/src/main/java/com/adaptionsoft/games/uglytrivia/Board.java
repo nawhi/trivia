ec74a67d864a1d3bbd8a13a482d00c77eb785a52
Nick <25394965+nawhi@users.noreply.github.com>
2019-05-09 12:25:25 +0100

    Clean up Board

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
@@ -4,39 +4,21 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Board {
-    int[] places = new int[6];
-    private Map<Player, Integer> playerPlaces = new HashMap<>();
+class Board {
+    private Map<Player, Integer> positions = new HashMap<>();
 
-    public Board() {
-    }
-
-    public Board(Player... players) {
+    Board(Player... players) {
         Arrays.stream(players)
-                .forEach(player -> playerPlaces.put(player, 0));
-    }
-
-    void initialisePlayerAt(int playerId) {
-        places[playerId] = 0;
-    }
-
-    void movePlayer(int roll, int playerId) {
-        places[playerId] = places[playerId] + roll;
-        if (places[playerId] > 11)
-            places[playerId] = places[playerId] - 12;
+                .forEach(player -> positions.put(player, 0));
     }
 
     void movePlayer(Player player, int roll) {
-        int currentPlace = playerPlaces.get(player);
+        int currentPlace = positions.get(player);
         int newPlace = (currentPlace + roll) % 12;
-        playerPlaces.put(player, newPlace);
-    }
-
-    int getPlayerPlace(int playerId) {
-        return places[playerId];
+        positions.put(player, newPlace);
     }
 
     int getPlayerPlace(Player player) {
-        return playerPlaces.get(player);
+        return positions.get(player);
     }
 }
