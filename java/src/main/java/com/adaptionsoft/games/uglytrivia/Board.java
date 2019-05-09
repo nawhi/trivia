b1a3ad2fe403ef62a0947be79a02d73040fc4228
Nick <25394965+nawhi@users.noreply.github.com>
2019-05-09 11:27:33 +0100

    Rename index -> id in Board

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
@@ -9,17 +9,17 @@ public class Board {
     public Board(Player... players) {
     }
 
-    void initialisePlayerAt(int playerIndex) {
-        places[playerIndex] = 0;
+    void initialisePlayerAt(int playerId) {
+        places[playerId] = 0;
     }
 
-    void movePlayer(int roll, int currentPlayerIndex) {
-        places[currentPlayerIndex] = places[currentPlayerIndex] + roll;
-        if (places[currentPlayerIndex] > 11)
-            places[currentPlayerIndex] = places[currentPlayerIndex] - 12;
+    void movePlayer(int roll, int playerId) {
+        places[playerId] = places[playerId] + roll;
+        if (places[playerId] > 11)
+            places[playerId] = places[playerId] - 12;
     }
 
-    int getPlayerPlace(int currentPlayerIndex) {
-        return places[currentPlayerIndex];
+    int getPlayerPlace(int playerId) {
+        return places[playerId];
     }
 }
